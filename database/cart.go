f4d2b0f1a30f4efd7ca967864d534a99598baad7
kaykobadhossain <kaykobadhossaintanjil01@gmail.com>
2025-09-18 10:51:22 +0600

    ...

---
+++ database/cart.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("can't find the product")
+	ErrCantDecodeProducts = errors.New("can't find the product")
+	ErrUserIdIsNotValid   = errors.New("this user is not valid")
+	ErrCantUpdateUser     = errors.New("can't add this product to the cart")
+	ErrCantRemoveItemCart = errors.New("can't remove this product from the cart")
+	ErrCantGetItem        = errors.New("was unable to get the item from the cart")
+	ErrCantBuyCartItem    = errors.New("can't update the purchase")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+func BuyItemFromCart() {
+
+}
+
+func InsatantBuyer() {
+
+}
